1675f5ea73be5950e5538b3105b90131dd5846ae
wonseok-han <oshan1112@gmail.com>
2021-07-15 15:13:25 +0900

    import 정렬 prettier, eslint 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,25 +12,28 @@ const OVERRIDE_ESLINT = {
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'import', 'simple-import-sort'],
   overrides: [OVERRIDE_ESLINT],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   rules: {
     'react/jsx-filename-extension': ['error', { extensions: ['.ts', '.tsx'] }],
-    'no-unused-vars': 'error', // 사용하지 않는 변수 선언 금지
+    'no-unused-vars': 'off', // 사용하지 않는 변수 선언 금지
     '@typescript-eslint/no-implied-eval': 'error', // eval 사용금지
     'no-duplicate-imports': 'error', // 중복 Import 금지
-    'no-extra-parens': 'error', // 불필요한 괄호 금지
     '@typescript-eslint/prefer-optional-chain': ['error'], // && 대신 OptionalChaining 표현식을 사용하는 것을 선호합니다.
-    '@typescript-eslint/no-for-in-array': ['off'],
+    'react/react-in-jsx-scope': 'off', // JSX를 사용할 때 누락된 React 방지
+    'simple-import-sort/imports': 'error',
   },
+  ignorePatterns: ['node_modules/'],
   settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
     'import/resolver': {
       node: {
         extensions: ['.js', '.ts', '.tsx', '.d.ts'],
