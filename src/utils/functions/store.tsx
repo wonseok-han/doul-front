989d5825eaa2031f43eb48c1d1decccd0f0eee44
wonseok-han <oshan1112@gmail.com>
2022-04-01 13:45:30 +0900

    sessionStorage get, set, remove 함수 추가

--- src/utils/functions/store.tsx
+++ src/utils/functions/store.tsx
@@ -15,3 +15,30 @@ export const setLocalStorage = (key: string, value: string): void => {
 export const getLocalStorage = (key: string): string | null | undefined => {
   return window.localStorage.getItem(key);
 };
+
+/**
+ * NOTE: Session Storage에 값 셋팅하는 함수
+ * @param key 셋팅할 키
+ * @param value 셋팅할 값
+ */
+export const setSessionStorage = (key: string, value: string): void => {
+  window.sessionStorage.setItem(key, value);
+};
+
+/**
+ * NOTE: Session Storage에서 Key Parameter에 해당하는 값을 얻어오는 함수
+ * @param key
+ * @returns
+ */
+export const getSessionStorage = (key: string): string | null | undefined => {
+  return window.sessionStorage.getItem(key);
+};
+
+/**
+ * NOTE: Session Storage에서 Key Parameter에 해당하는 항목을 제거하는 함수
+ * @param key
+ * @returns
+ */
+export const removeSessionStorage = (key: string): void => {
+  window.sessionStorage.removeItem(key);
+};
