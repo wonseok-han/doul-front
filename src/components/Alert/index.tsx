cf5f5dee1fdae8b8c4940142ce68c0a81a9ac0ca
wonseok-han <oshan1112@gmail.com>
2022-04-04 09:33:01 +0900

    Alert 기본 스타일 적용

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,7 +1,10 @@
+import "./styles.scss";
+
+import classNames from "classnames";
 import Button from "components/Button";
 import React from "react";
 import { Modal } from "react-bootstrap";
-import { actions, useAppContext } from "utils/context";
+import { actions, useAppContext, useThemeContext } from "utils/context";
 
 export interface AlertProps {
   show?: boolean;
@@ -17,6 +20,7 @@ const Alert: React.FC<AlertProps> = ({
   callBack,
 }: AlertProps) => {
   const { dispatch } = useAppContext();
+  const { store: themeStore } = useThemeContext();
   const { showAlert } = actions;
 
   // NOTE: Alert Close 이벤트
@@ -28,14 +32,25 @@ const Alert: React.FC<AlertProps> = ({
   return (
     <>
       <Modal
+        className={classNames("modal")}
         show={show}
         onHide={handleClose}
         backdrop={"static"}
         keyboard={false}
       >
         {header && <Modal.Header>{header}</Modal.Header>}
-        <Modal.Body>{body}</Modal.Body>
-        <Modal.Footer>
+        <Modal.Body
+          className={classNames("modal-body", {
+            darkMode: themeStore?.darkMode,
+          })}
+        >
+          {body}
+        </Modal.Body>
+        <Modal.Footer
+          className={classNames("modal-footer", {
+            darkMode: themeStore?.darkMode,
+          })}
+        >
           <Button onClick={handleClose}>닫기</Button>
         </Modal.Footer>
       </Modal>
