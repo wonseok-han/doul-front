4530435196c4a2153c0b667bf5c86cfdaf679717
wonseok-han <oshan1112@gmail.com>
2022-04-05 17:57:24 +0900

    404페이지 스타일 적용

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,8 +1,38 @@
+import "./styles.scss";
+
+import classNames from "classnames";
+import Button from "components/Button";
+import { useNavigate } from "react-router-dom";
+import { useThemeContext } from "utils/context";
+
 const NotFoundPage: React.FC = () => {
+  const { store: themeStore } = useThemeContext();
+  const navigate = useNavigate();
+
   return (
-    <div style={{ padding: 24, margin: 0, minHeight: 280 }}>
-      <p style={{ textAlign: "center" }}>Page Not Found</p>
-    </div>
+    <main
+      className={classNames("not-found-main", {
+        darkMode: themeStore?.darkMode,
+      })}
+    >
+      <h1
+        className={classNames("not-found-h1", {
+          darkMode: themeStore?.darkMode,
+        })}
+      >
+        404
+      </h1>
+      <p
+        className={classNames("not-found-p", {
+          darkMode: themeStore?.darkMode,
+        })}
+      >
+        Page Not Found
+      </p>
+      <Button style={{ marginTop: 40 }} onClick={() => navigate("/")}>
+        Go Home
+      </Button>
+    </main>
   );
 };
 export default NotFoundPage;
